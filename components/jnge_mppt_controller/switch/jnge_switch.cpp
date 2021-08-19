50d2702f3297f9d568964412b10441f8fc9c33be
Sebastian Muszynski <s.muszynski@constructiva.de>
2021-08-19 21:07:02 +0200

    Fix typo

--- components/jnge_mppt_controller/switch/jnge_switch.cpp
+++ components/jnge_mppt_controller/switch/jnge_switch.cpp
@@ -7,7 +7,7 @@ namespace jnge_mppt_controller {
 
 static const char *const TAG = "jnge_mppt_controller.switch";
 
-void JngeSwitch::dump_config() { LOG_SWITCH("", "Jnge_MpptController Switch", this); }
+void JngeSwitch::dump_config() { LOG_SWITCH("", "JngeMpptController Switch", this); }
 void JngeSwitch::write_state(bool state) { this->parent_->write_register(this->holding_register_, (uint16_t) state); }
 
 }  // namespace jnge_mppt_controller
