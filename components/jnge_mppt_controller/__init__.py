50c54d3f3b56fc4f9c36f6a19538c10ef9357817
Sebastian Muszynski <s.muszynski@constructiva.de>
2022-03-06 15:13:39 +0100

    Fix yaml

--- components/jnge_mppt_controller/__init__.py
+++ components/jnge_mppt_controller/__init__.py
@@ -3,7 +3,7 @@ import esphome.config_validation as cv
 from esphome.components import jnge_modbus
 from esphome.const import CONF_ID
 
-AUTO_LOAD = ["jnge_modbus", "binary_sensor", "sensor", "switch", "text_sensor"]
+AUTO_LOAD = ["jnge_modbus", "binary_sensor", "sensor", "switch", "text_sensor", "output"]
 CODEOWNERS = ["@syssi"]
 MULTI_CONF = True
 
