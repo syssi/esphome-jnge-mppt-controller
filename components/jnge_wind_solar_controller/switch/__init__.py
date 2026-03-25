3df738fddda693ea8d57a2002f4f3bc5dc94b1a3
Sebastian Muszynski <basti@linkt.de>
2026-03-25 17:06:28 +0100

    Use new_* API instead of deprecated register_* functions
    Replace register_binary_sensor/register_switch/register_button/register_number/register_select
    with the modern new_* API. The new_* functions internally call cg.new_Pvariable() + register_*(),
    reducing boilerplate. register_component() is kept separately for Component subclasses.

--- components/jnge_wind_solar_controller/switch/__init__.py
+++ components/jnge_wind_solar_controller/switch/__init__.py
@@ -1,7 +1,6 @@
 import esphome.codegen as cg
 from esphome.components import switch
 import esphome.config_validation as cv
-from esphome.const import CONF_ID
 
 from .. import (
     JNGE_WIND_SOLAR_CONTROLLER_COMPONENT_SCHEMA,
@@ -52,9 +51,8 @@ async def to_code(config):
     for key, address in SWITCHES.items():
         if key in config:
             conf = config[key]
-            var = cg.new_Pvariable(conf[CONF_ID])
+            var = await switch.new_switch(conf)
             await cg.register_component(var, conf)
-            await switch.register_switch(var, conf)
             cg.add(getattr(hub, f"set_{key}_switch")(var))
             cg.add(var.set_parent(hub))
             cg.add(var.set_holding_register(address))
